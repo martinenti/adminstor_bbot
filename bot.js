a4e890107b4073815d7635deec8c4bbd3bd1db27
martinenti <40662211+martinenti@users.noreply.github.com>
2019-01-26 02:25:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,4 +15,27 @@ client.on('message', msg => {
     msg.reply('البوت شغال تمام');
   }
 });
+
+if(cmd ==="-report"){
+        let wUser = message.guild.member (message.mentions.users.first())  message.guild.members.get(args[0]);
+          if(!wUser) return message.reply("يجب ان تمنشن شخص اول")
+    let reason = args.join(" ").slice(22);
+
+message.channel.send("تم ارسال  الشكوى  الى الادارة العليا  ")
+
+   let embed = new Discord.RichEmbed()
+          .setColor('RANDOM')
+  .addField("reoprt user",${wUser} with id ${wUser.id})
+  .addField("reoprt by",${message.author} with id ${message.author.id})
+  .addField("channel",message.channel)
+  .addField("TIME",message.createdAt)
+  .addField("Reson",reason)
+
+
+  let warnchannel = message.guild.channels.find(name,"report")
+  if(!warnchannel) return message.reply("لا يجود الشات ")
+
+  message.delete().catch(O_o=>{})
+  warnchannel.send(embed);
+
   client.login(process.env.BOT_TOKEN);
