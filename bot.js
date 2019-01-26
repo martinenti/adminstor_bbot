43c2512f169b0c20c00b36c7771650cff2162153
martinenti <40662211+martinenti@users.noreply.github.com>
2019-01-26 02:16:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,12 +2,17 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as Codes List .#2551!`);
+  console.log(`WELCOME MR_MARTIN I'M YOUR BOT NOW I'M ONLINE!`);
 });
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
     msg.reply('Pong!');
+  }
+});
+client.on('message', msg => {
+  if (msg.content === 'test') {
+    msg.reply('البوت شغال تمام');
   }
 });
   client.login(process.env.BOT_TOKEN);
